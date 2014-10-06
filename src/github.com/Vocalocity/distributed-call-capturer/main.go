077ee4c69afcf4b6492868a66cf064e6b84f7700
denodaeus <bobby.smith@vocalocity.com>
2014-10-06 14:35:10 -0400

    PT-1: ... convention

--- src/github.com/Vocalocity/distributed-call-capturer/main.go
+++ src/github.com/Vocalocity/distributed-call-capturer/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 import "flag"
 
-//import "gopcap" "github.com/akrennmair/gopcap"
+import "github.com/akrennmair/gopcap"
 
 func main() {
 	var mode = flag.String("mode", "client", "Mode of execution:  server (handle commands) or client (capture data)")
