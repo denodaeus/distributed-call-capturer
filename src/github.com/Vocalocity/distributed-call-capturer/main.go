9908bd2501719de49b04e4b39542758163536e4d
denodaeus <bobby.smith@vocalocity.com>
2014-10-06 13:27:06 -0400

    PT-1: moving path to convention

---
+++ src/github.com/Vocalocity/distributed-call-capturer/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "flag"
+
+//import "gopcap" "github.com/akrennmair/gopcap"
+
+func main() {
+	var mode = flag.String("mode", "client", "Mode of execution:  server (handle commands) or client (capture data)")
+	flag.Parse()
+	fmt.Println("mode", *mode)
+}
--- src/vocalocity.com/distributed-call-capturer/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-import "gopcap" "github.com/akrennmair/gopcap"
-
-func main() {
-}
