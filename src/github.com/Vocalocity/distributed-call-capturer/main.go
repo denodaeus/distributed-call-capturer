b5ab7234127c67e9c95c3a8ff807ad1d001ede15
denodaeus <bobby.smith@vocalocity.com>
2014-10-10 15:49:54 -0400

    PT-1: defining server type

--- src/github.com/Vocalocity/distributed-call-capturer/main.go
+++ src/github.com/Vocalocity/distributed-call-capturer/main.go
@@ -3,10 +3,19 @@ package main
 import "fmt"
 import "flag"
 
-import "github.com/akrennmair/gopcap"
-
 func main() {
 	var mode = flag.String("mode", "client", "Mode of execution:  server (handle commands) or client (capture data)")
 	flag.Parse()
 	fmt.Println("mode", *mode)
+
+	switch *mode {
+	case "client":
+		fmt.Println("initializing " + *mode)
+	case "server":
+		fmt.Println("initializing " + *mode)
+		s := Server{}
+		s.init()
+	default:
+		panic("Unable to initialize for mode " + *mode)
+	}
 }
--- src/github.com/Vocalocity/distributed-call-capturer/server.go
+++ src/github.com/Vocalocity/distributed-call-capturer/server.go
@@ -1,1 +1,9 @@
 package main
+
+import "fmt"
+
+type Server struct{}
+
+func (s Server) init() {
+	fmt.Println("Initializing server daemon for RPC communication ...")
+}
