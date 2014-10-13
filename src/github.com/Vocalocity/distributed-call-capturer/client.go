d87d7d0119e51c4a5d6383462c9b22fbe5207bc5
denodaeus <bobby.smith@vocalocity.com>
2014-10-13 10:50:36 -0400

    PRX-3: lint

--- src/github.com/Vocalocity/distributed-call-capturer/client.go
+++ src/github.com/Vocalocity/distributed-call-capturer/client.go
@@ -2,8 +2,11 @@ package main
 
 import "fmt"
 
+// Client is the daemon representing a capture client.  It receives commands to queue tasks from the running
+// server daemon and is configured to register a keep alive mechanism and report to it.
 type Client struct{}
 
+//init creates a new client agent to listen on configured bind settings
 func (c Client) init() {
 	fmt.Println("Initializing client daemon for RPC communication ...")
 }
--- src/github.com/Vocalocity/distributed-call-capturer/server.go
+++ src/github.com/Vocalocity/distributed-call-capturer/server.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
-/* Server daemon type */
+// Server is the server daemon used to query current running jobs and submit new capture jobs.
+// The Server is also the entity that capture clients register with to keep distributed tasks in sync.
 type Server struct{}
 
+//init creates a new server agent to listen on default configuration settings
 func (s Server) init() {
 	fmt.Println("Initializing server daemon for RPC communication ...")
 }
